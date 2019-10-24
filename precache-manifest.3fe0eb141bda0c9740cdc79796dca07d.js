6ccddc7749ff0a20379de72e083719657a89033d
exegate <der.shoxed@gmail.com>
2019-10-24 15:01:14 +0300

    Updates

---
+++ precache-manifest.3fe0eb141bda0c9740cdc79796dca07d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef85f33a90812d140e5d601af349b740",
+    "url": "/scr_r/index.html"
+  },
+  {
+    "revision": "6f45fadaea6ae6871957",
+    "url": "/scr_r/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b47ab6b38a0ef8b1d6fa",
+    "url": "/scr_r/static/js/2.604df13d.chunk.js"
+  },
+  {
+    "revision": "6f45fadaea6ae6871957",
+    "url": "/scr_r/static/js/main.c82a6b66.chunk.js"
+  },
+  {
+    "revision": "95b7608ad5efad6093c3",
+    "url": "/scr_r/static/js/runtime-main.a1eda044.js"
+  }
+]);
